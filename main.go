e3275751b419db6abbe6d0f57a970da2f651ddc7
yuanzix <achubadyal4@gmail.com>
2024-08-15 17:41:51 +0530

    main: correct conditional statement for env error

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	_, err2 := utils.ReadJWTSecret()
 	_, _, err3 := utils.ReadGmailDetails()
 	_, err4 := utils.ReadBackendURL()
-	if err1 != nil && err2 != nil && err3 != nil && err4 != nil {
+	if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
 		log.Fatal(err1, err2, err3, err4)
 	}
 
